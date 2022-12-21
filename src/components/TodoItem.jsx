bf1a6ecfc7c0a516452e8179c897ea548ed9b19f
arch-spatula <shkim516@naver.com>
2022-12-21 21:04:07 +0900

    feat, moveItem #7
    todo Item의 상태를 다음단계로 전환하는 기능을 추가했습니다.
    확장성을 고려하여 kindObj.js를 추가했습니다.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
-import { deleteTodoThunk } from "../redux/modules/todoSlice";
+import { deleteTodoThunk, moveTodoThunk } from "../redux/modules/todoSlice";
+import { shiftTodo, shiftTodoText } from "../shared/kindObj";
 
 /**
  * @param {*} param0
@@ -10,12 +11,22 @@ const TodoItem = ({ id, kind, content, title }) => {
   const handleDelete = (id) => {
     dispatch(deleteTodoThunk(id));
   };
+  const handleMove = (kind) => {
+    const updateItem = {
+      id,
+      content,
+      title,
+      kind: shiftTodo[kind],
+    };
+    dispatch(moveTodoThunk(updateItem));
+  };
   return (
     <div>
       <h3>{title}</h3>
       <p>{content}</p>
       <div>
         <button onClick={() => handleDelete(id)}>삭제</button>
+        <button onClick={() => handleMove(kind)}>{shiftTodoText[kind]}</button>
         {/* 완료 : 취소 */}
       </div>
     </div>
