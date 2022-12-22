13ae9364dfb0a417e6c3bd82844ed26387df9486
arch-spatula <shkim516@naver.com>
2022-12-22 23:30:34 +0900

    ETC, CSS reset #8
    임시 CSS reset입니다. CustomInput을 만들고 완전히 CSS RESET합니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { TodoList, CustomForm, Header } from "./components";
+import Layout from "./shared/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <CustomForm />
-      <TodoList heading="Working" kind="Working" />
-      <TodoList heading="Done..." kind="Done" />
+      <Layout>
+        <Header />
+        <CustomForm />
+        <TodoList heading="Working" kind="Working" />
+        <TodoList heading="Done..." kind="Done" />
+      </Layout>
     </div>
   );
 }
